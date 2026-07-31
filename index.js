6ff792ce8671e60c7fa3a0c7168bdd949f1ec95a
Gify Samuel A <gifysamuelamarth@gmail.com>
2026-07-31 12:59:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        document
+        .getElementById("loader")
+        .classList
+        .add("loader-hidden");
+
+    }, 1900);
+
+});
